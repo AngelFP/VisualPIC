e6e8d238e55cff807d21d07390d432dd58a6472d
AngelFP <angelmail_97@msn.com>
2017-01-24 09:11:05 +0100

    Updated scripts for Windows complilation.

--- Compile/Windows/WindowsCompile.bat
+++ Compile/Windows/WindowsCompile.bat
@@ -1,4 +1,5 @@
 pyinstaller VisualPIC.spec
 RD /S /Q "build"
 cd dist
+DEL /Q "VisualPIC.zip"
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('VisualPIC', 'VisualPIC.zip'); }"
