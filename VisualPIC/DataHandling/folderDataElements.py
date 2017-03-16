cb7f4e96cf829f8429f4374e26cd89380b764b82
AngelFP <angelmail_97@msn.com>
2017-03-17 00:31:09 +0100

    Speed optmizations for 3D visualizaion from 2D fields.

--- VisualPIC/DataHandling/folderDataElements.py
+++ VisualPIC/DataHandling/folderDataElements.py
@@ -101,11 +101,11 @@ class FolderField(FolderDataElement):
         field3D = np.zeros((transvEl, transvEl, longEl))
         X, Y, Z = np.mgrid[0:cilShape[0]:transvSpacing,0:cilShape[0]:transvSpacing,0:cilShape[1]:lonSpacing] # cart. coordinates of 3D field
         Rout = np.sqrt(X**2 + Y**2)
-        field3D[int(transvEl/2):transvEl+1,int(transvEl/2):transvEl+1] = ip.griddata(np.column_stack((Rin,Zin)), field2D, (Rout, Z)) # top right section when looking back from z to the x-y plane.
+        field3D[int(transvEl/2):transvEl+1,int(transvEl/2):transvEl+1] = ip.griddata(np.column_stack((Rin,Zin)), field2D, (Rout, Z), method='nearest', fill_value = 0) # top right section when looking back from z to the x-y plane.
         field3D[0:int(transvEl/2),int(transvEl/2):transvEl+1] = np.flip(field3D[int(transvEl/2):transvEl+1,int(transvEl/2):transvEl+1], 0)
         field3D[0:int(transvEl/2),0:int(transvEl/2)] = np.flip(field3D[0:int(transvEl/2),int(transvEl/2):transvEl+1], 1)
         field3D[int(transvEl/2):transvEl+1,0:int(transvEl/2)] = np.flip(field3D[int(transvEl/2):transvEl+1,int(transvEl/2):transvEl+1], 1)
-        return np.nan_to_num(field3D)
+        return field3D
     
     """
     Get data in any units
--- VisualPIC/Tools/visualizer3Dvtk.py
+++ VisualPIC/Tools/visualizer3Dvtk.py
@@ -178,6 +178,7 @@ class Visualizer3Dvtk():
     def GetVTKWidget(self, parentWidget):
         self.vtkWidget = QVTKRenderWindowInteractor(parentWidget)
         self.renderer = vtk.vtkRenderer()
+        #self.renderer.SetBackground(1,1,1)
         self.vtkWidget.GetRenderWindow().AddRenderer(self.renderer)
         self.interactor = self.vtkWidget.GetRenderWindow().GetInteractor()
         self.interactor.Initialize()
@@ -218,13 +219,13 @@ class Visualizer3Dvtk():
         volumeprop.IndependentComponentsOn()
         volumeprop.SetInterpolationTypeToLinear()
         for i, volume in enumerate(self.volumeList):
-            npdatauchar.append(volume.GetData(timeStep, 250, 500)) # limit on elements only applies for 2d case
+            npdatauchar.append(volume.GetData(timeStep, 200, 100)) # limit on elements only applies for 2d case
             volumeprop.SetColor(i,volume.color)
             volumeprop.SetScalarOpacity(i,volume.opacity)
             volumeprop.ShadeOff(i)
         npdatamulti = np.concatenate([aux[...,np.newaxis] for aux in npdatauchar], axis=3)
         axes = self.volumeList[0].GetAxes(timeStep)
-        axesSpacing = self.volumeList[0].GetAxesSpacing(timeStep, 250, 500) # limit on elements only applies for 2d case
+        axesSpacing = self.volumeList[0].GetAxesSpacing(timeStep, 200, 100) # limit on elements only applies for 2d case
         # Put data in VTK format
         dataImport = vtk.vtkImageImport()
         dataImport.SetImportVoidPointer(npdatamulti)
