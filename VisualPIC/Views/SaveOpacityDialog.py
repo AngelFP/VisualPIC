205c16bdb4015de0d1c42438a7470c977defd8be
AngelFP <angelmail_97@msn.com>
2018-05-25 17:25:33 +0200

    Create python file for new dialog

---
+++ VisualPIC/Views/SaveOpacityDialog.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+#Copyright 2016-2018 Angel Ferran Pousa, DESY
+#
+#This file is part of VisualPIC.
+#
+#VisualPIC is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#VisualPIC is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with VisualPIC.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import os
+
+from PyQt5.uic import loadUiType
+
+
+if getattr(sys, 'frozen', False):
+    # we are running in a bundle
+    bundle_dir = sys._MEIPASS
+else:
+    # we are running in a normal Python environment
+    bundle_dir = os.path.dirname(os.path.abspath(__file__))
+guipath = os.path.join( bundle_dir, 'SaveOpacityDialog.ui' )
+Ui_SaveOpacityDialog, SaveOpacityDialog = loadUiType(guipath)
+
+
+class SaveOpacityDialog(QSaveOpacityDialog, Ui_SaveOpacityDialog):
+    def __init__(self, parent=None):
+        super(SaveOpacityDialog, self).__init__(parent)
+        self.setupUi(self)
