3f06a08dcdab46fb4238e5d6118de8dd24b1c5ff
AngelFP <angelmail_97@msn.com>
2018-06-08 10:27:50 +0200

    Added missing package data

--- setup.py
+++ setup.py
@@ -6,15 +6,19 @@ def read_requirements():
         return [line.strip() for line in f.readlines()]
 
 # Main setup command
-setup(name='visualPIC',
+setup(name='VisualPIC',
       description='Data visualizer for PIC codes',
       url='https://github.com/AngelFP/VisualPIC',
       maintainer='Angel Ferran Pousa',
       license='GPL',
       packages=find_packages('.'),
-      package_data={'VisualPIC': ['Views/*.ui']},
+      package_data={'VisualPIC': ['Views/*.ui',
+                                  'Assets/Visualizer3D/Colormaps/*.h5',
+                                  'Assets/Visualizer3D/Opacities/*.h5',
+                                  'Icons/*.png',
+                                  'Icons/mpl/*.svg']},
       entry_points={
           'console_scripts': [
-              'visualPIC = VisualPIC.__main__:display_gui']},
+              'visualpic = VisualPIC.__main__:display_gui']},
       install_requires=read_requirements(),
       platforms='any')
