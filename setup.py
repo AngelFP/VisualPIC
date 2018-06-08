8be8abf87e183cbce90baa2fc18cbf4ede24927f
AngelFP <angelmail_97@msn.com>
2018-06-08 16:40:09 +0200

    Add more details to setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,24 @@ from setuptools import setup, find_packages
 
 import VisualPIC
 
+# Read long description
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 def read_requirements():
     with open('requirements.txt') as f:
         return [line.strip() for line in f.readlines()]
 
 # Main setup command
 setup(name='VisualPIC',
-      description='Data visualizer for PIC codes',
       version=VisualPIC.__version__,
+      author='Angel Ferran Pousa',
+      author_email="angel.ferran.pousa@desy.de",
+      description='Data visualizer for PIC codes',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/AngelFP/VisualPIC',
-      maintainer='Angel Ferran Pousa',
-      license='GPL',
+      license='GPLv3',
       packages=find_packages('.'),
       package_data={'VisualPIC': ['Views/*.ui',
                                   'Assets/Visualizer3D/Colormaps/*.h5',
@@ -24,4 +31,15 @@ setup(name='VisualPIC',
           'console_scripts': [
               'visualpic = VisualPIC.__main__:display_gui']},
       install_requires=read_requirements(),
-      platforms='any')
+      platforms='any',
+      classifiers=(
+          "Development Status :: 3 - Alpha",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Intended Audience :: Science/Research",
+          "Topic :: Scientific/Engineering :: Physics",
+          "Topic :: Scientific/Engineering :: Visualization",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Operating System :: OS Independent"),
+      )
