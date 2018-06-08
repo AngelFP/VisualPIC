87ba08e7e503b5f04f5a92d4f49d581118239f70
AngelFP <angelmail_97@msn.com>
2018-06-08 14:31:23 +0200

    Add module imports again

--- VisualPIC/__init__.py
+++ VisualPIC/__init__.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
+import __main__
 from .__version__ import __version__
+from . import Controls
+from . import DataHandling
+from . import DataPlotting
+from . import DataReading
+from . import Views
+
