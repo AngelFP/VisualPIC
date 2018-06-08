f04c7fe87739322a8b59e3163f4c7b84929bcc13
AngelFP <angelmail_97@msn.com>
2018-06-08 14:15:04 +0200

    Added __version__

--- VisualPIC/__init__.py
+++ VisualPIC/__init__.py
@@ -1,9 +1,2 @@
 # -*- coding: utf-8 -*-
-import __main__
-from . import Controls
-from . import DataHandling
-from . import DataPlotting
-from . import DataReading
-from . import Views
-
-
+from .__version__ import __version__
