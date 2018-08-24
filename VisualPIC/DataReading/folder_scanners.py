bea000b8ecdb1e9ab6773e3a1f8d60374ddecc81
AngelFP <angelmail_97@msn.com>
2018-08-24 17:01:57 +0200

    Implement field scanner for osiris

--- VisualPIC/DataReading/folder_scanners.py
+++ VisualPIC/DataReading/folder_scanners.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+
+#Copyright 2016-2018 Angel Ferran Pousa, DESY
+#
+#This file is part of VisualPIC.
+#
+#VisualPIC is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#VisualPIC is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with VisualPIC.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import os
 
@@ -22,25 +41,38 @@ class OsirisFolderScanner(FolderScanner):
         folders_in_path = os.listdir(folder_path)
         for folder in folders_in_path:
             if folder == "DENSITY":
-                pass
+                subdir = os.path.join(folder_path, folder)
+                available_species = os.listdir(subdir)
+                for species in available_species:
+                    species_folder = os.path.join(subdir, species)
+                    if os.path.isdir(species_folder):
+                        species_fields = os.listdir(species_folder)
+                        for field in species_fields:
+                            field_folder = os.path.join(species_folder, field)
+                            if os.path.isdir(field_folder):
+                                field_list.append(self._create_field(
+                                    field, field_folder, species))
             if folder == "FLD":
                 subdir = os.path.join(folder_path, folder)
                 domain_fields = os.listdir(subdir)
                 for field in domain_fields:
                     field_folder = os.path.join(subdir, field)
                     if os.path.isdir(field_folder):
-                        field_path = self._get_field_path(field)
-                        osiris_field_name = self._get_osiris_field_name(
-                            field)
-                        field_name = self._get_standard_visualpic_name(
-                            osiris_field_name)
-                        fld_files, time_steps = self._get_files_and_timesteps(
-                            field_folder)
                         field_list.append(
-                            FolderField(field_name, field_path, fld_files,
-                                        time_steps, self.field_reader,
-                                        'uc'))
+                            self._create_field(field, field_folder))
         return field_list
+
+    def _create_field(self, field_name, field_folder, species_name=""):
+        field_path = self._get_field_path(field_name)
+        osiris_field_name = self._get_osiris_field_name(
+            field_name)
+        field_name = self._get_standard_visualpic_name(
+            osiris_field_name)
+        fld_files, time_steps = self._get_files_and_timesteps(
+            field_folder)
+        return FolderField(field_name, field_path, fld_files,
+                        time_steps, self.field_reader,
+                        'uc', species_name)
 
     def _get_field_path(self, field_folder_name):
         return '/' + self._get_osiris_field_name(field_folder_name)
