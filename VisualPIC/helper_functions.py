ad86e33ad2bd9bf1e65a7ef57a62f55f3af49e96
AngelFP <angelmail_97@msn.com>
2018-12-17 19:48:05 +0100

    Add helper function

---
+++ VisualPIC/helper_functions.py
@@ -0,0 +1,23 @@
+"""This module contains methods needed by other modules"""
+
+
+def join_infile_path(*paths):
+    """
+    Join path components using '/' as separator.
+    This method is defined as an alternative to os.path.join, which uses '\\'
+    as separator in Windows environments and is therefore not valid to navigate
+    within data files.
+
+    Parameters:
+    -----------
+    *paths: all strings with path components to join
+
+    Returns:
+    --------
+    A string with the complete path using '/' as separator.
+    """
+    # Join path components
+    path = '/'.join(paths)
+    # Correct double slashes, if any is present
+    path = path.replace('//', '/')
+    return path
