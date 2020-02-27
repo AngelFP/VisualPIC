630a847a56187c5eae545639db23be6dd5087c7c
AngelFP <angelmail_97@msn.com>
2020-02-27 11:15:28 +0100

    Move volume appearance classes to separate module. Change vtk opacity implementation.

--- VisualPIC/visualization/vtk_visualizer.py
+++ VisualPIC/visualization/vtk_visualizer.py
@@ -1,19 +1,18 @@
 """This module contains the classes for 3D visualization with VTK"""
 
-import os
 import sys
 from pkg_resources import resource_filename
 
 import vtk
 import numpy as np
 from scipy.ndimage import zoom
-from h5py import File as H5File
 try:
     from PyQt5 import QtWidgets
     qt_installed = True
 except:
     qt_installed = False
 
+from VisualPIC.visualization.volume_appearance import *
 if qt_installed:
     from VisualPIC.visualization.basic_render_window import BasicRenderWindow
 
@@ -388,6 +387,7 @@ class VolumetricField():
         self.ytrim = ytrim
         self.ztrim = ztrim
         self.resolution = resolution
+        self.vtk_opacity = vtk.vtkPiecewiseFunction()
 
     def get_name(self):
         return field.field_name
@@ -411,7 +411,6 @@ class VolumetricField():
         return ax_orig, ax_spacing
 
     def get_vtk_opacity(self, timestep=None):
-        vtk_opacity = vtk.vtkPiecewiseFunction()
         if isinstance(self.opacity, Opacity):
             opacity = self.opacity
         elif self.opacity != 'auto':
@@ -422,10 +421,18 @@ class VolumetricField():
                     "Opacity '{}' does not exist.".format(self.opacity))
         else:
             opacity = self.get_optimized_opacity(timestep)
+        self._set_vtk_opacity(opacity)
+        return self.vtk_opacity
+
+    def set_opacity(self, opacity):
+        self.opacity = opacity
+        self._set_vtk_opacity(opacity)
+
+    def _set_vtk_opacity(self, opacity):
         fld_vals, op_vals = opacity.get_opacity_values()
+        self.vtk_opacity.RemoveAllPoints()
         for fv, ov in zip(fld_vals, op_vals):
-            vtk_opacity.AddPoint(fv, ov)
-        return vtk_opacity
+            self.vtk_opacity.AddPoint(fv, ov)
 
     def get_vtk_colormap(self):
         vtk_cmap = vtk.vtkColorTransferFunction()
@@ -522,249 +529,3 @@ class VolumetricField():
             z = zoom(z, z_zoom)
         return x, y, z
 
-
-class VolumeStyleHandler():
-
-    """Defines a style handler as singleton."""
-
-    vsh = None
-    
-    def __init__(self):
-        if VolumeStyleHandler.vsh is None:
-            VolumeStyleHandler.vsh = VolumeStyleHandler.__VolumeStyleHandler()
-
-    def __getattr__(self, name):
-        return getattr(self.vsh, name)
-
-    class __VolumeStyleHandler():
-        def __init__(self):
-            self.max_len = 50
-            self.opacity_folder_path = resource_filename(
-                'VisualPIC.Assets.Visualizer3D.Opacities', '')
-            self.cmaps_folder_path = resource_filename(
-                'VisualPIC.Assets.Visualizer3D.Colormaps', '')
-            self.initialize_available_opacities()
-            self.initialize_available_cmaps()
-
-        """Opacities"""
-        def initialize_available_opacities(self):
-            self.default_opacities = list()
-            self.other_opacities = list()
-            folder_opacities = self.get_opacities_in_default_folder()
-            if len(folder_opacities) > 0:
-                self.default_opacities += (
-                    self.get_opacities_in_default_folder())
-            else:
-                self.default_opacities.append(self.create_fallback_opacity())
-
-        def get_opacities_in_default_folder(self):
-            files_in_folder = os.listdir(self.opacity_folder_path)
-            folder_opacities = list()
-            for file in files_in_folder:
-                if file.endswith('.h5'):
-                    file_path = self.create_file_path(file, 
-                                                      self.opacity_folder_path)
-                    folder_opacities.append(Opacity(file_path))
-            return folder_opacities
-
-        def create_fallback_opacity(self):
-            n_points = 11
-            name = "linear positive"
-            fld_vals = np.linspace(0, 255, n_points)
-            op_vals = np.linspace(0, 0.999, n_points)
-            op = Opacity(name=name, fld_values=fld_vals, op_values=op_vals)
-            return op
-
-        def get_available_opacities(self):
-            ops = list()
-            for op in self.default_opacities + self.other_opacities:
-                ops.append(op.get_name())
-            return ops
-        
-        def opacity_exists(self, op_name):
-            for op in self.default_opacities + self.other_opacities:
-                if op.get_name() == op_name:
-                    return True
-            return False
-
-        def get_opacity(self, op_name):
-            for op in self.default_opacities + self.other_opacities:
-                if op.get_name() == op_name:
-                    return op
-
-        def get_opacity_data(self, op_name):
-            for op in self.default_opacities + self.other_opacities:
-                if op.get_name() == op_name:
-                    return op.get_opacity_values()
-
-        def save_opacity(self, name, field_values, opacity_values,
-                         folder_path):
-            if (field_values.min() >= 0 and field_values.max() <= 255
-                and opacity_values.min() >= 0 and opacity_values.max() <= 1
-                and len(field_values) == len(opacity_values)
-                and len(opacity_values) <= self.max_len):
-                file_path = self.create_file_path(name, folder_path)
-                # Create H5 file
-                file = H5File(file_path,  "w")
-                opacity_dataset = file.create_dataset(
-                    "opacity", data = opacity_values)
-                field_dataset = file.create_dataset("field",
-                                                    data = field_values)
-                file.attrs["opacity_name"] = name
-                file.close()
-                # Add to available opacities
-                opacity = Opacity(file_path)
-                if (os.path.normpath(folder_path) 
-                    == self.opacity_folder_path):
-                    self.default_opacities.append(opacity)
-                else:
-                    self.other_opacities.append(opacity)
-                return True
-            else:
-                return False
-
-        def add_opacity_from_file(self, file_path):
-            self.other_opacities.append(Opacity(file_path))
-        
-        
-        """Colormaps"""
-        def initialize_available_cmaps(self):
-            self.default_cmaps = list()
-            self.other_cmaps = list()
-            folder_cmaps = self.get_cmaps_in_default_folder()
-            if len(folder_cmaps) > 0:
-                self.default_cmaps += (
-                    self.get_cmaps_in_default_folder())
-            else:
-                self.default_cmaps.append(self.create_fallback_cmap())
-
-        def get_cmaps_in_default_folder(self):
-            files_in_folder = os.listdir(self.cmaps_folder_path)
-            folder_cmaps = list()
-            for file in files_in_folder:
-                if file.endswith('.h5'):
-                    file_path = self.create_file_path(file, 
-                                                      self.cmaps_folder_path)
-                    folder_cmaps.append(Colormap(file_path))
-            return folder_cmaps
-
-        def create_fallback_cmap(self):
-            n_points = 11
-            cmap = Colormap("")
-            cmap.name = "default"
-            cmap.fld_data = np.linspace(0, 255, n_points)
-            r_1 = np.linspace(0, 1, (n_points+1) / 2)
-            r_2 = np.linspace(1, 0, (n_points+1) / 2)
-            cmap.r_data = np.append(np.delete(r_1, -1), r_2)
-            cmap.g_data = np.linspace(0, 1, n_points)
-            cmap.b_data = np.linspace(1, 0, n_points)
-            return cmap
-
-        def get_available_cmaps(self):
-            cmaps = list()
-            for cmap in self.default_cmaps + self.other_cmaps:
-                cmaps.append(cmap.get_name())
-            return cmaps
-        
-        def cmap_exists(self, cmap_name):
-            for cmap in self.default_cmaps + self.other_cmaps:
-                if cmap.get_name() == cmap_name:
-                    return True
-            return False
-
-        def get_cmap(self, cmap_name):
-            for cmap in self.default_cmaps + self.other_cmaps:
-                if cmap.get_name() == cmap_name:
-                    return cmap
-
-        def get_cmap_data(self, cmap_name):
-            for cmap in self.default_cmaps + self.other_cmaps:
-                if cmap.get_name() == cmap_name:
-                    return cmap.get_cmap()
-
-        def save_cmap(self, name, fld_val, r_val, g_val, b_val, folder_path):
-            if (fld_val.min() >= 0 and fld_val.max() <= 255
-                and r_val.min() >= 0 and r_val.max() <= 255
-                and g_val.min() >= 0 and g_val.max() <= 255
-                and b_val.min() >= 0 and b_val.max() <= 255
-                and len(fld_val) == len(r_val) == len(g_val) == len(b_val)
-                and len(fld_val) <= self.max_len):
-                file_path = self.create_file_path(name, folder_path)
-                # Create H5 file
-                file = H5File(file_path,  "w")
-                r_dataset = file.create_dataset("r", data=r_val)
-                g_dataset = file.create_dataset("g", data=g_val)
-                b_dataset = file.create_dataset("b", data=b_val)
-                fld_dataset = file.create_dataset("field", data=fld_val)
-                file.attrs["cmap_name"] = name
-                file.close()
-                # Add to available colormaps
-                cmap = Colormap(file_path)
-                if (os.path.normpath(folder_path) == self.cmaps_folder_path):
-                    self.default_cmaps.append(cmap)
-                else:
-                    self.other_cmaps.append(cmap)
-                return True
-            else:
-                return False
-        
-        def add_cmap_from_file(self, file_path):
-            self.other_cmaps.append(Colormap(file_path))
-
-        """Common"""
-        def create_file_path(self, file_name, folder_path):
-            if not file_name.endswith('.h5'):
-                file_name += ".h5"
-                file_name = file_name.replace(' ', '_').lower()
-            file_path = os.path.join(folder_path, file_name)
-            return file_path
-
-
-class Opacity():
-
-    """Class defining an opacity map fr 3D fields."""
-
-    def __init__(self, file_path=None, name=None, op_values=None,
-                 fld_values=None):
-        if file_path is not None:
-            file = H5File(file_path, "r")
-            self.name = file.attrs["opacity_name"]
-            self.op_values = np.array(file["/opacity"])
-            self.fld_values = np.array(file["/field"])
-        else:
-            self.name = name
-            self.op_values = op_values
-            self.fld_values = fld_values
-
-    def get_name(self):
-        return self.name
-
-    def get_opacity_values(self):
-        return self.fld_values, self.op_values
-
-
-class Colormap():
-    
-    """Class defining a colormap for 3D fields."""
-
-    def __init__(self, file_path=None, name=None, r_values=None, g_values=None,
-                 b_values=None, fld_values=None):
-        if file_path is not None:
-            file = H5File(file_path, "r")
-            self.name = file.attrs["cmap_name"]
-            self.r_values = np.array(file["/r"])
-            self.g_values = np.array(file["/g"])
-            self.b_values = np.array(file["/b"])
-            self.fld_values = np.array(file["/field"])
-        else:
-            self.name = name
-            self.r_values = r_values
-            self.g_values = g_values
-            self.b_values = b_values
-            self.fld_values = fld_values
-
-    def get_name(self):
-        return self.name
-
-    def get_cmap_values(self):
-        return self.fld_values, self.r_values, self.g_values, self.b_values
