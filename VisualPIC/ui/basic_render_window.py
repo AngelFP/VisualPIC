69c3134327bdb7c4b3cbf4e8b1c83a680e64b877
AngelFP <angelmail_97@msn.com>
2020-03-17 17:44:45 +0100

    Polish ui and implement functionality

--- VisualPIC/ui/basic_render_window.py
+++ VisualPIC/ui/basic_render_window.py
@@ -1,10 +1,15 @@
+from pkg_resources import resource_filename
+
 import vtk
+import numpy as np
 from PyQt5.Qt import Qt
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, QtGui
 from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
 from VisualPIC.ui.setup_field_volume_window import SetupFieldVolumeWindow
 from VisualPIC.ui.brightness_contrast_dialog import BrightnessContrastDialog
+from VisualPIC.helper_functions import (
+    get_closest_timestep, get_previous_timestep, get_next_timestep)
 
 
 class BasicRenderWindow(QtWidgets.QMainWindow):
@@ -14,6 +19,7 @@ class BasicRenderWindow(QtWidgets.QMainWindow):
     def __init__(self, vtk_visualizer, parent=None):
         super().__init__(parent=parent)
         self.vtk_vis = vtk_visualizer
+        self.available_timesteps = self.vtk_vis.get_possible_timesteps()
         self.setup_interface()
         self.register_ui_events()
         self.show()
@@ -31,12 +37,41 @@ class BasicRenderWindow(QtWidgets.QMainWindow):
         self.vl.addWidget(self.vtk_widget)
 
         self.hl = QtWidgets.QHBoxLayout()
+        self.timestep_line_edit = QtWidgets.QLineEdit()
+        self.timestep_line_edit.setReadOnly(True)
+        self.timestep_line_edit.setAlignment(Qt.AlignCenter)
+        self.timestep_line_edit.setText(str(self.vtk_vis.current_time_step))
+        self.timestep_line_edit.setMaximumWidth(50)
+        self.hl.addWidget(self.timestep_line_edit)
+        self.prev_button = QtWidgets.QPushButton()
+        prev_icon_path = resource_filename('VisualPIC.ui.icons',
+                                           'left_arrow.png')
+        self.prev_button.setIcon(QtGui.QIcon(prev_icon_path))
+        self.hl.addWidget(self.prev_button)
+        self.next_button = QtWidgets.QPushButton()
+        next_icon_path = resource_filename('VisualPIC.ui.icons',
+                                           'right_arrow.png')
+        self.next_button.setIcon(QtGui.QIcon(next_icon_path))
+        self.hl.addWidget(self.next_button)
         self.timestep_slider = QtWidgets.QSlider(Qt.Horizontal)
+        self.timestep_slider.setRange(np.min(self.available_timesteps),
+                                      np.max(self.available_timesteps))
+        self.timestep_slider.setValue(self.vtk_vis.current_time_step)
         self.hl.addWidget(self.timestep_slider)
         self.edit_fields_button = QtWidgets.QPushButton()
-        self.edit_fields_button.setText('Edit fields')
+        # Icon by Garrett Knoll, from The Noun Project, CC BY 3.0 us,
+        # https://commons.wikimedia.org/w/index.php?curid=49665075
+        edit_icon_path = resource_filename('VisualPIC.ui.icons', 'edit.svg')
+        self.edit_fields_button.setIcon(QtGui.QIcon(edit_icon_path))
         self.settings_button = QtWidgets.QPushButton()
-        self.settings_button.setText('Settings')
+        # Icon by
+        # https://phabricator.wikimedia.org/diffusion/GOJU/browse/master/
+        # AUTHORS.txt 
+        # - lib/oojs-ui/themes/mediawiki/images/icons/, MIT,
+        # https://commons.wikimedia.org/w/index.php?curid=54913197
+        settings_icon_path = resource_filename('VisualPIC.ui.icons',
+                                               'slider_settings.svg')
+        self.settings_button.setIcon(QtGui.QIcon(settings_icon_path))
         self.hl.addWidget(self.edit_fields_button)
         self.hl.addWidget(self.settings_button)
         
@@ -47,6 +82,10 @@ class BasicRenderWindow(QtWidgets.QMainWindow):
     def register_ui_events(self):
         self.edit_fields_button.clicked.connect(self.edit_fields_button_clicked)
         self.settings_button.clicked.connect(self.settings_button_clicked)
+        self.prev_button.clicked.connect(self.prev_button_clicked)
+        self.next_button.clicked.connect(self.next_button_clicked)
+        self.timestep_slider.sliderReleased.connect(self.timestep_slider_released)
+        self.timestep_slider.valueChanged.connect(self.timestep_slider_value_changed)
 
     def edit_fields_button_clicked(self):
         fld_list = self.vtk_vis.get_list_of_fields()
@@ -66,6 +105,33 @@ class BasicRenderWindow(QtWidgets.QMainWindow):
         if self.settings_dialog is None:
             self.settings_dialog = BrightnessContrastDialog(self.vtk_vis, self)
         self.settings_dialog.show()
+
+    def prev_button_clicked(self):
+        current_ts = self.timestep_slider.value()
+        prev_ts = get_previous_timestep(current_ts, self.available_timesteps)
+        if prev_ts != current_ts:
+            self.timestep_slider.setValue(prev_ts)
+        self.render_timestep(prev_ts)
+
+    def next_button_clicked(self):
+        current_ts = self.timestep_slider.value()
+        next_ts = get_next_timestep(current_ts, self.available_timesteps)
+        if next_ts != current_ts:
+            self.timestep_slider.setValue(next_ts)
+        self.render_timestep(next_ts)
+
+    def timestep_slider_released(self):
+        value = self.timestep_slider.value()
+        value = get_closest_timestep(value, self.available_timesteps)
+        self.timestep_slider.setValue(value)
+        self.render_timestep(value)
+
+    def timestep_slider_value_changed(self, value):
+        self.timestep_line_edit.setText(str(value))
+
+    def render_timestep(self, timestep):
+        self.vtk_vis.make_timestep_render(timestep, ts_is_index=False)
+        self.interactor.Render()
 
     def closeEvent(self, *args, **kwargs):
         """ This fixes bug described in 
