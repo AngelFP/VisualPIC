b9854af04a599ba1cadc27283d43c58bd95a85b4
AngelFP <angelmail_97@msn.com>
2020-03-20 13:24:52 +0100

    Remove data caching

--- VisualPIC/data_handling/fields.py
+++ VisualPIC/data_handling/fields.py
@@ -29,36 +29,12 @@ class Field():
         self.timesteps = field_timesteps
         self.species_name = species_name
         self.unit_converter = unit_converter
-        self._current_data_params = None
-        self._fld_data = None
-        self._fld_metadata = None
 
     def get_data(self, time_step, field_units=None, axes_units=None,
                  axes_to_convert=None, time_units=None, slice_i=0.5,
                  slice_j=0.5, slice_dir_i=None, slice_dir_j=None, m='all',
                  theta=0, max_resolution_3d_tm=None, only_metadata=False):
-        params = [time_step, field_units, axes_units, axes_to_convert,
-                  time_units, slice_i, slice_j, slice_dir_i, slice_dir_j, m,
-                  theta, max_resolution_3d_tm]
-        # Avoid reading data more than once in a consecutive way
-        if (params != self._current_data_params or
-            (not only_metadata and self._fld_data is None)):
-            self._current_data_params = params
-            fld_data, fld_metadata = self._get_data(
-                time_step, field_units=field_units, axes_units=axes_units,
-                axes_to_convert=axes_to_convert, time_units=time_units,
-                slice_dir_i=slice_dir_i, slice_dir_j=slice_dir_j, m=m,
-                theta=theta, max_resolution_3d_tm=max_resolution_3d_tm,
-                only_metadata=only_metadata)
-            if only_metadata:
-                self._fld_data = None
-            else:
-                self._fld_data = fld_data
-            self._fld_metadata = fld_metadata
-        if only_metadata:
-            return [], copy(self._fld_metadata)
-        else:
-            return copy(self._fld_data), copy(self._fld_metadata)
+        raise NotImplementedError
 
     def get_only_metadata(self, time_step, field_units=None, axes_units=None,
                           axes_to_convert=None, time_units=None,
@@ -76,12 +52,6 @@ class Field():
         field_md = self.get_only_metadata(self.timesteps[0])
         return field_md['field']['geometry']
 
-    def _get_data(self, time_step, field_units=None, axes_units=None,
-                  time_units=None, slice_i=0.5, slice_j=0.5, slice_dir_i=None,
-                  slice_dir_j=None, m='all', theta=0,
-                  max_resolution_3d_tm=None, only_metadata=False):
-        raise NotImplementedError
-
 
 class FolderField(Field):
     def __init__(self, field_name, field_path, field_files, field_timesteps,
@@ -92,10 +62,10 @@ class FolderField(Field):
         self.field_files = field_files
         self.field_reader = field_reader
 
-    def _get_data(self, time_step, field_units=None, axes_units=None,
-                  axes_to_convert=None, time_units=None, slice_i=0.5,
-                  slice_j=0.5, slice_dir_i=None, slice_dir_j=None, m='all',
-                  theta=0, max_resolution_3d_tm=None, only_metadata=False):
+    def get_data(self, time_step, field_units=None, axes_units=None,
+                 axes_to_convert=None, time_units=None, slice_i=0.5,
+                 slice_j=0.5, slice_dir_i=None, slice_dir_j=None, m='all',
+                 theta=0, max_resolution_3d_tm=None, only_metadata=False):
         file_path = self._get_file_path(time_step)
         fld, fld_md = self.field_reader.read_field(
             file_path, self.field_path, slice_i, slice_j, slice_dir_i,
@@ -125,10 +95,10 @@ class DerivedField(Field):
         unit_converter = base_fields[0].unit_converter
         super().__init__(field_name, field_timesteps, unit_converter)
 
-    def _get_data(self, time_step, field_units=None, axes_units=None,
-                  axes_to_convert=None, time_units=None, slice_i=0.5,
-                  slice_j=0.5, slice_dir_i=None, slice_dir_j=None, m='all',
-                  theta=0, max_resolution_3d_tm=None, only_metadata=False):
+    def get_data(self, time_step, field_units=None, axes_units=None,
+                 axes_to_convert=None, time_units=None, slice_i=0.5,
+                 slice_j=0.5, slice_dir_i=None, slice_dir_j=None, m='all',
+                 theta=0, max_resolution_3d_tm=None, only_metadata=False):
         field_data = []
         for field in self.base_fields:
             fld, fld_md = field.get_data(
